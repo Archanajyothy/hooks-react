8ea7974809e17f4fee03d910ebbf6d477fcbed85
archanajyothy <archanajyothy@gmail.com>
2023-05-21 22:02:48 +0530

    useReducer hook practice

--- hooks-app/src/App.js
+++ hooks-app/src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import StatePractice from './UseState/statePractice';
 import StatePractice2 from './UseState/statePractice2';
 import StatePractice3 from './UseState/statePractice3';
+import ReducerTutorial from './UseReducer/ReducerTutorial';
 
 function App() {
   return (
     <div className="App">
       {/* <StatePractice />
-      <StatePractice2 /> */}
-      <StatePractice3 />
+      <StatePractice2 /> 
+      <StatePractice3 /> */}
+      <ReducerTutorial />
     </div>
   );
 }
