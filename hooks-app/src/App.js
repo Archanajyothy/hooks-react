5ab08c61d84997fe5d6b3ff0d21da57f3ca1d775
archanajyothy <archanajyothy@gmail.com>
2023-05-21 13:54:52 +0530

    useState practice

--- hooks-app/src/App.js
+++ hooks-app/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import StatePractice from './UseState/statePractice';
 import StatePractice2 from './UseState/statePractice2';
+import StatePractice3 from './UseState/statePractice3';
 
 function App() {
   return (
     <div className="App">
-      <StatePractice />
-      <StatePractice2 />
+      {/* <StatePractice />
+      <StatePractice2 /> */}
+      <StatePractice3 />
     </div>
   );
 }
